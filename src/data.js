96a54ba7e76e7d418753f95b12ef5eae53b4d7b4
Ricardo Gastón Pereyra <rgpxen@gmail.com>
2019-01-16 00:29:26 -0300

    AÑADIR: archivo data.js

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const meses= [
+    {
+        id: 1,
+        nombres: "Enero",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 2,
+        nombres: "Febrero",
+        dias: 28,
+        cumple: []
+    },
+    {
+        id: 3,
+        nombres: "Marzo",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 4,
+        nombres: "Abril",
+        dias: 30,
+        cumple: []
+    },
+    {
+        id: 5,
+        nombres: "Mayo",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 6,
+        nombres: "Junio",
+        dias: 30,
+        cumple: []
+    },
+    {
+        id: 7,
+        nombres: "Julio",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 8,
+        nombres: "Agosto",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 9,
+        nombres: "Septiembre",
+        dias: 30,
+        cumple: []
+    },
+    {
+        id: 10,
+        nombres: "Octubre",
+        dias: 31,
+        cumple: []
+    },
+    {
+        id: 11,
+        nombres: "Noviembre",
+        dias: 30,
+        cumple: []
+    },
+    {
+        id: 12,
+        nombres: "Diciembre",
+        dias: 31,
+        cumple: []
+    }
+];
